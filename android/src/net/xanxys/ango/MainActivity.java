1cd9f14e84caaa91ea874bc2941ec1de8adaf4e8
xanxys <xanxys@gmail.com>
2014-01-24 14:22:16 +0900

    Add camera view.

--- android/src/net/xanxys/ango/MainActivity.java
+++ android/src/net/xanxys/ango/MainActivity.java
@@ -2,22 +2,71 @@ package net.xanxys.ango;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
+	private Camera camera;
+	private CameraView cameraView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		camera = getCameraInstance();
+		cameraView = new CameraView(this, camera);
+		FrameLayout layout = (FrameLayout) findViewById(R.id.camera_view);
+		layout.addView(cameraView);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (this.camera == null) {
+			camera = getCameraInstance();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (camera != null) {
+			camera.release();
+			camera = null;
+		}
+	}
+
+	private boolean checkCameraHardware(Context context) {
+		if (context.getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			// this device has a camera
+			return true;
+		} else {
+			// no camera on this device
+			return false;
+		}
+	}
+
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
 }
